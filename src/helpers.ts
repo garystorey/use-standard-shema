c4a4f2cce50633756f0cc2785476422bc1b81303
Gary Storey <garystorey655@gmail.com>
2025-08-04 12:21:00 -0400

    fix defaultValue being required

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,53 +1,65 @@
-import type { FormValues, Schema, SchemaField } from "./types"
+import type { FormValues, Schema, SchemaField } from "./types";
 
 export function defineSchema<T extends Schema>(schema: T): T {
-	return schema
+  return schema;
 }
 
 export function isSchemaField(obj: unknown): obj is SchemaField {
-	return typeof obj === "object" && obj !== null && "label" in obj && "defaultValue" in obj && "schema" in obj
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "label" in obj &&
+    "defaultValue" in obj &&
+    "schema" in obj
+  );
 }
 
 export function toFormData(data: FormValues) {
-	const formData = new FormData()
-	Object.entries(data).forEach(([key, value]) => {
-		if (value !== undefined && value !== null) {
-			formData.append(key, String(value))
-		}
-	})
-	return formData
+  const formData = new FormData();
+  Object.entries(data).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      formData.append(key, String(value));
+    }
+  });
+  return formData;
 }
 
-export function flattenSchema(map: Schema, prefix = ""): Record<string, SchemaField> {
-	const result: Record<string, SchemaField> = {}
+export function flattenSchema(
+  map: Schema,
+  prefix = ""
+): Record<string, SchemaField> {
+  const result: Record<string, SchemaField> = {};
 
-	for (const key in map) {
-		const value = map[key]
-		const path = prefix ? `${prefix}.${key}` : key
+  for (const key in map) {
+    const value = map[key];
+    const path = prefix ? `${prefix}.${key}` : key;
 
-		if (isSchemaField(value)) {
-			result[path] = value
-		} else {
-			Object.assign(result, flattenSchema(value, path))
-		}
-	}
+    if (isSchemaField(value)) {
+      result[path] = value;
+    } else {
+      Object.assign(result, flattenSchema(value, path));
+    }
+  }
 
-	return result
+  return result;
 }
 
-export function flattenDefaults(map: Schema, prefix = ""): Record<string, string> {
-	const result: Record<string, string> = {}
+export function flattenDefaults(
+  map: Schema,
+  prefix = ""
+): Record<string, string> {
+  const result: Record<string, string> = {};
 
-	for (const key in map) {
-		const value = map[key]
-		const path = prefix ? `${prefix}.${key}` : key
+  for (const key in map) {
+    const value = map[key];
+    const path = prefix ? `${prefix}.${key}` : key;
 
-		if (isSchemaField(value)) {
-			result[path] = value.defaultValue
-		} else {
-			Object.assign(result, flattenDefaults(value, path))
-		}
-	}
+    if (isSchemaField(value)) {
+      result[path] = value.defaultValue ?? "";
+    } else {
+      Object.assign(result, flattenDefaults(value, path));
+    }
+  }
 
-	return result
+  return result;
 }
